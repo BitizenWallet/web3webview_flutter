5c415fc96fbfce0f8b0f7d7b83a7854033922a63
naiba <hi@nai.ba>
2022-02-28 17:32:07 +0800

    chore(codegen): pack injection scripts

---
+++ scripts/pack.sh
@@ -0,0 +1,5 @@
+echo "import 'dart:convert';
+
+String injectJs = utf8.fuse(base64).decode(\"`cat assets/inject.js | base64`\");
+String injectJsBundle = Uri.encodeComponent(utf8.fuse(base64).decode(\"`cat assets/index.js | base64`\"));
+" > lib/assets.dart
